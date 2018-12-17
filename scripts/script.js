39d4fee5ffc5ca5fa2cfe6a00c271c0e3f80c45b
clairevalant <valantclaire@gmail.com>
2018-12-17 10:53:38 -0500

    trying to fix the hamburger nav

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,29 @@ myApp.smoothScroll = function () {
 }
 
 myApp.nav = () => {
-    $(".menu").on("click", function() {
-        $(this).toggleClass("turn");
-        $("nav").toggleClass("open");
+    $(".menu").on("click", () => {        
+        $(".menu").toggleClass("turn");
+
+        // different actions if nav is open or closed
+        $(".siteNav").hasClass("open") ? myApp.closeNav() : myApp.openNav()
     })
+}
+
+// get halp with this
+myApp.openNav = () => {
+    $(".siteNav").toggleClass("open closed");
+    $(".open").animate({
+        width: "70vw"
+    });
+}
+
+myApp.closeNav = () => {
+    $(".siteNav").animate({
+        width: "0vw",
+    });
+    // can i add a then?? no?
+    $(".siteNav").toggleClass("open closed");
+    $(".menu").toggleClass("turn");
 }
 
 myApp.init = () => {
